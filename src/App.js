d02a2613a940d3cb46d1359a68566994b9d0b55f
Riya Jose Pratheeksh <rjosepratheeksh@gmail.com>
2021-09-08 08:11:09 -0500

    Api Axions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
+import {useState} from 'react';
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  const onClickHandler = () => {
+    console.log("clicked");
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then(res => {
+        setPokemon(res.data.results);
+      })
+      .catch(err => console.log(err))
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hey it's me</h1>
+      <button onClick={onClickHandler} className="btn btn-warning btn-lg">Click me</button>
+    <ol>
+      {
+        pokemon.map((item, i)=>{
+          return <li key={i}>{item.name}</li>
+        })
+      }
+    </ol>
+
     </div>
   );
 }
